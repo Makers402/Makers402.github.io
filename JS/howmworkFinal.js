6ba584fd14516bbd23764206e532ffdd33e1085d
Makers402 <32500720+Makers402@users.noreply.github.com>
2018-01-30 15:09:22 -0600

    lots of work

--- JS/howmworkFinal.js
+++ JS/howmworkFinal.js
@@ -1,7 +1,12 @@
-function addListItem() {
-    var text = $("#newListItem").val();
+/*function addListItem() {
+    var text = $("#newListItem").val().addClass();
     $("#grocery").append('<ul>'+text+'</ul>');
 }
 $(function(){
    $("#addTask").on('click', addListItem); 
+});*/
+$("#addTask").click(function(e){
+   var TaskName = $("#newListItem").val();
+    var listOfChoice = $("#taskList").val();
+    $("#"+listOfChoice).append("")
 });
